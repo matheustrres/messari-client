6fc5aac7422dc6b0f968ca4a519b2152da523e70
Matheus Torres <torresmiranda.rj@gmail.com>
2023-07-31 19:37:22 -0300

    refactor(test): rewrite tests more assertively

--- tests/client.spec.ts
+++ tests/client.spec.ts
@@ -1,4 +1,5 @@
 import { MessariClient } from '../lib/client';
+import { MessariAssetMarketData } from '../lib/typings';
 
 describe('MessariClient', (): void => {
   let client: MessariClient;
@@ -13,102 +14,139 @@ describe('MessariClient', (): void => {
     expect(client).toBeDefined();
   });
 
-  describe('getAsset', (): void => {
+  describe('.getAsset', (): void => {
     it('should get basic metadata for an asset', async (): Promise<void> => {
-      const asset = await client.getAsset('ethereum');
+      const { status, data } = await client.getAsset('bitcoin');
 
-      expect(asset.status.error_code).toBeUndefined();
-      expect(asset.data).toBeDefined();
-      expect(asset.data!.id).toBeDefined();
-      expect(asset.data!.name).toBe('Ethereum');
-      expect(asset.data!.symbol).toBe('ETH');
-      expect(asset.data!.slug).toBe('ethereum');
+      expect(status.error_code).toBe(undefined);
+      expect(status.error_message).toBe(undefined);
+
+      expect(data).toBeDefined();
+      expect(data!.id).toBeDefined();
+      expect(data!.serial_id).toBe(6057);
+      expect(data!.symbol).toBe('BTC');
+      expect(data!.name).toBe('Bitcoin');
+      expect(data!.slug).toBe('bitcoin');
     });
   });
 
-  describe('getAssetMarketData', (): void => {
-    it('should get the market data for an asset', async (): Promise<void> => {
-      const marketData = await client.getAssetMarketData('ethereum');
+  describe('.getAssetMetrics', (): void => {
+    it('should get all the quantitative metrics for an asset', async (): Promise<void> => {
+      const { status, data } = await client.getAssetMetrics('Polkadot');
 
-      expect(marketData.status.error_code).toBeUndefined();
-      expect(marketData.data).toBeDefined();
-      expect(marketData.data!.market_data).toBeDefined();
+      expect(status.error_code).toBe(undefined);
+      expect(status.error_message).toBe(undefined);
+
+      expect(data!.id).toBeDefined();
+      expect(data!.serial_id).toBe(373);
+      expect(data!.symbol).toBe('DOT');
+      expect(data!.name).toBe('Polkadot');
+      expect(data!.slug).toBe('polkadot');
+      expect(data!.market_data).toBeDefined();
+      expect(data!.marketcap).toBeDefined();
+      expect(data!.reddit).toBeDefined();
+      expect(data!.all_time_high).toBeDefined();
+      expect(data!.roi_data).toBeDefined();
     });
   });
 
-  describe('getAssetMetrics', (): void => {
-    it('should get an asset metrics', async (): Promise<void> => {
-      const metrics = await client.getAssetMetrics('ethereum');
+  describe('.getAssetMarketData', (): void => {
+    it('should get the latest market-data for an asset.', async (): Promise<void> => {
+      type MyAssetMarketData = MessariAssetMarketData & {
+        Asset: {
+          id: string;
+          name: string;
+          symbol: string;
+          slug: string;
+          serial_id: number;
+        };
+      };
 
-      expect(metrics.status.error_code).toBeUndefined();
-      expect(metrics.data).toBeDefined();
-      expect(metrics.data!.id).toBeDefined();
-      expect(metrics.data!.name).toBeDefined();
-      expect(metrics.data!.market_data).toBeDefined();
-      expect(metrics.data!.market_data).toBeDefined();
-      expect(metrics.data!.marketcap).toBeDefined();
+      const { status, data } = await client.getAssetMarketData<MyAssetMarketData>('ethereum');
+
+      expect(status.error_code).toBe(undefined);
+      expect(status.error_message).toBe(undefined);
+
+      expect(data!.Asset).toBeDefined();
+      expect(data!.Asset.id).toBeDefined();
+      expect(data!.Asset.serial_id).toBe(6059);
+      expect(data!.Asset.symbol).toBe('ETH');
+      expect(data!.Asset.name).toBe('Ethereum');
+      expect(data!.Asset.slug).toBe('ethereum');
+
+      expect(data!.market_data).toBeDefined();
+      expect(data!.market_data.price_usd).toBeDefined();
+      expect(data!.market_data.price_eth).toBeDefined();
+      expect(data!.market_data.price_btc).toBeDefined();
+      expect(data!.market_data.ohlcv_last_1_hour).toBeDefined();
+      expect(data!.market_data.ohlcv_last_24_hour).toBeDefined();
     });
   });
 
-  describe('listAllAssets', (): void => {
-    it('should list all assets', async (): Promise<void> => {
-      const assets = await client.listAllAssets();
-
-      expect(assets.status.error_code).toBeUndefined();
-      expect(assets.data).toBeDefined();
-      expect(assets.data![0].id).toBeDefined();
-      expect(assets.data![0].name).toBeDefined();
-      expect(assets.data![0].metrics).toBeDefined();
-    });
-
-    it('should list only 5 assets', async (): Promise<void> => {
-      const assets = await client.listAllAssets({
-        limit: 5,
+  describe('.listAllAssets', (): void => {
+    it('should get the paginated list of all assets and their metrics', async (): Promise<void> => {
+      const { status, data } = await client.listAllAssets({
+        limit: 6,
+        page: 2,
       });
 
-      expect(assets.status.error_code).toBeUndefined();
-      expect(assets.data).toBeDefined();
-      expect(assets.data!.length).toBe(5);
-      expect(assets.data![0].id).toBeDefined();
-      expect(assets.data![5]).toBeFalsy();
+      expect(status.error_code).toBe(undefined);
+      expect(status.error_message).toBe(undefined);
+
+      expect(data).toBeDefined();
+      expect(data!.length).toBe(6);
+      expect(data![0].id).toBeDefined();
+      expect(data![1].id).toBeDefined();
+      expect(data![2].id).toBeDefined();
+      expect(data![3].id).toBeDefined();
+      expect(data![4].id).toBeDefined();
+      expect(data![5].id).toBeDefined();
+      expect(data![6]).toBe(undefined);
     });
   });
 
-  describe('listAllNews', (): void => {
-    it('should list all news and analysis for all assets', async (): Promise<void> => {
-      const allNews = await client.listAllAssetsNews();
-
-      expect(allNews.status.error_code).toBeUndefined();
-      expect(allNews.data).toBeDefined();
-      expect(allNews.data![0].id).toBeDefined();
-      expect(allNews.data![0].title).toBeDefined();
-      expect(allNews.data![0].content).toBeDefined();
-    });
-
-    it('should list only 5 news and analysis for all assets', async (): Promise<void> => {
-      const news = await client.listAllAssetsNews({
-        limit: 5,
+  describe('.listAllAssetsNews', (): void => {
+    it('should get the paginated list of latest news and analysis for all assets', async (): Promise<void> => {
+      const { status, data } = await client.listAllAssetsNews({
+        limit: 3,
+        page: 2,
       });
-      
-      expect(news.status.error_code).toBeUndefined();
-      expect(news.data).toBeDefined();
-      expect(news.data!.length).toBe(5);
-      expect(news.data![0].author).toBeDefined();
-      expect(news.data![5]).toBeFalsy();
+
+      expect(status.error_code).toBe(undefined);
+      expect(status.error_message).toBe(undefined);
+
+      expect(data).toBeDefined();
+      expect(data![0].id).toBeDefined();
+      expect(data![0].title).toBeDefined();
+      expect(data![1].id).toBeDefined();
+      expect(data![1].title).toBeDefined();
+      expect(data![2].id).toBeDefined();
+      expect(data![2].title).toBeDefined();
+      expect(data![3]).toBe(undefined);
     });
   });
 
-  describe('listAssetNews', (): void => {
-    it('should list news for an asset', async (): Promise<void> => {
-      const assetNews = await client.listAssetNews('ethereum');
+  describe('.listAssetNews', (): void => {
+    it.only('should get the paginated list of latest news and analysis for an asset', async (): Promise<void> => {
+      const { status, data } = await client.listAssetNews('ethereum', {
+        limit: 4,
+      });
 
-      expect(assetNews.status.error_code).toBeUndefined();
-      expect(assetNews.data).toBeDefined();
-      expect(assetNews.data![0].id).toBeDefined();
-      expect(assetNews.data![0].title).toBeDefined();
-      expect(assetNews.data![0].content).toBeDefined();
+      expect(status.error_code).toBe(undefined);
+      expect(status.error_message).toBe(undefined);
+
+      expect(data).toBeDefined();
+      expect(data![0].id).toBeDefined();
+      expect(data![0].title).toBeDefined();
+      expect(data![1].id).toBeDefined();
+      expect(data![1].title).toBeDefined();
+      expect(data![2].id).toBeDefined();
+      expect(data![2].title).toBeDefined();
+      expect(data![3].id).toBeDefined();
+      expect(data![3].title).toBeDefined();
+      expect(data![4]).toBe(undefined);
     });
-  })
+  });
 
   it('should fail when searching for an invalid asset', async (): Promise<void> => {
     await expect(client.getAsset('fake-asset').then(res => res.status.error_code)).resolves.toBeDefined();
