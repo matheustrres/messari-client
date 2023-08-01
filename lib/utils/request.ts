1071d2083affaa2eaf7fc269c0755e8b295d036e
Matheus <torresmiranda.rj@gmail.com>
2023-07-31 23:22:02 -0300

    refactor: use native fetch instead of undici as http client

--- lib/utils/request.ts
+++ lib/utils/request.ts
@@ -1,5 +1,3 @@
-import undici from 'undici';
-
 import { getRequestErrorMessage } from '../constraints/errors';
 import { QueryResult } from '../typings';
 import { MessariError } from './errors/messari.error';
@@ -9,23 +7,22 @@ type RequestConfigProps = {
 };
 
 export class Request {
-	private request: typeof undici.request;
-
-	constructor(private config: RequestConfigProps) {
-		this.request = undici.request;
-	}
+	constructor(private config: RequestConfigProps) {}
 
 	public async get<T extends QueryResult>(
 		endpoint: string,
 	): Promise<T | MessariError> {
-		const res = await this.request(`https://data.messari.io/api/${endpoint}`, {
-			method: 'GET',
-			headers: {
-				'x-messari-api-key': this.config.messariApiKey,
+		const res = await fetch(
+			new URL(`https://data.messari.io/api/${endpoint}`),
+			{
+				method: 'GET',
+				headers: {
+					'x-messari-api-key': this.config.messariApiKey,
+				},
 			},
-		});
+		);
 
-		const resObj = (await res.body.json()) as T;
+		const resObj = (await res.json()) as T;
 
 		if (resObj.status.error_code) {
 			const errorMessage: string = getRequestErrorMessage(
