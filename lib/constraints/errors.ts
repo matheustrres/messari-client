604eac6f64b4ea6ee6b309c924daf89c30d8c62f
Matheus <torresmiranda.rj@gmail.com>
2023-08-03 11:01:59 -0300

    refactor: use an object instead of raw consts & set a default error message

--- lib/constraints/errors.ts
+++ lib/constraints/errors.ts
@@ -1,21 +1,27 @@
-export const SUCCESSFUL_REQUEST = 'Request succeeded';
-export const INVALID_PARAM_CONSTRAINT = 'Invalid param value given';
-export const UNAUTHORIZED_CONSTRAINT = 'Unauthorized (authentication)';
-export const FORBIDDEN_CONSTRAINT = 'Forbidden (authorization)';
-export const NOT_FOUND_CONSTRAINT = 'Asset not found';
-export const RATE_LIMIT_CONSTRAINT = 'Too many requests (rate limit)';
-export const INTERNAL_SERVER_ERROR_CONSTRAINT =
-	'An Internal server error occurred';
+type ObjectValues<T> = T[keyof T];
+
+export const MessariRequestErrors = {
+	SUCCESSFUL: 'Request succeeded',
+	BAD_REQUEST: 'Bad request',
+	UNAUTHORIZED: 'Unauthorized (authentication)',
+	FORBIDDEN: 'Forbidden (authorization)',
+	NOT_FOUND: 'Asset not found',
+	TOO_MANY_REQUESTS: 'Too many requests (rate limit)',
+	INTERNAL_SERVER_ERROR: 'An Internal server error occurred',
+} as const;
+
+type MessariRequestErrors = ObjectValues<typeof MessariRequestErrors>;
 
 export const getRequestErrorMessage = (code: number): string => {
 	const errorCodes = {
-		400: INVALID_PARAM_CONSTRAINT,
-		401: UNAUTHORIZED_CONSTRAINT,
-		403: FORBIDDEN_CONSTRAINT,
-		404: NOT_FOUND_CONSTRAINT,
-		429: RATE_LIMIT_CONSTRAINT,
-		500: INTERNAL_SERVER_ERROR_CONSTRAINT,
+		200: MessariRequestErrors.SUCCESSFUL,
+		400: MessariRequestErrors.BAD_REQUEST,
+		401: MessariRequestErrors.UNAUTHORIZED,
+		403: MessariRequestErrors.FORBIDDEN,
+		404: MessariRequestErrors.NOT_FOUND,
+		429: MessariRequestErrors.TOO_MANY_REQUESTS,
+		500: MessariRequestErrors.INTERNAL_SERVER_ERROR,
 	};
 
-	return errorCodes[code];
+	return errorCodes[code] || 'Unknown error';
 };
